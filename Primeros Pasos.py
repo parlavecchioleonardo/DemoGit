8675ee71ac89b211bfbffb12ef72a425afa0b926
Leonardo Parlavecchio <parlavecchioleonardo@gmail.com>
2025-04-11 00:10:00 -0300

    1

--- Primeros Pasos.py
+++ Primeros Pasos.py
@@ -8,4 +8,8 @@ for i in range(5):
     a+=1
     print(a)
 
-print (9//2)
+miLista1=["Maria", "Pepe", "Marta", "Antonio"]
+miLista2=["Leo", "Estefi", "Mamá", "Snake"]
+miLista3=miLista1+miLista2
+
+print(miLista3)
