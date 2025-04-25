51e8dc991333bdcd967ba7287f673569a1220b23
Leonardo Parlavecchio <parlavecchioleonardo@gmail.com>
2025-04-25 16:21:32 -0300

    s

--- 11 test.py
+++ 11 test.py
@@ -1,13 +1,19 @@
-contrasena=input("Por favor introduzca su contraseña: ")
-lencontrasena=len(contrasena)
-vacio=0
+email=False
+miEmail=input("Por favor introduzca su dirección de correo: ")
+contadorpunto=0
+contadorarroba=0
 
-for i in contrasena:
-    if (i == " "):
-        vacio=vacio+1
-   
+for i in miEmail:
+     
+    if (i == "@"):
+        contadorarroba=contadorarroba+1
+    elif (i== "."):
+        contadorpunto=contadorpunto+1
+    
+    if contadorarroba==1 and contadorpunto>=1:
+        email=True
 
-if lencontrasena>=8 and vacio==0:
-    print("Ingreso correcto de contraseña")
-else:
-    print("Ingreso de contraseña incorrecto, la contraseña debe tener mas de 8 caracteres y ningun espacio vacio")
+if email: #Con poner solamente email ya evalua que sea True
+    print("Email es correcto")
+else:   
+    print("Email incorrecto")
