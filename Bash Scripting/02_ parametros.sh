a162a5dd2e08ee7581a7339ddd1c9469857c7e78
Leonardo Parlavecchio <parlavecchioleonardo@gmail.com>
2025-06-16 22:01:53 -0300

    l

--- Bash Scripting/02_ parametros.sh
+++ Bash Scripting/02_ parametros.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # Author: Leonardo Parlavecchio
 # Ejemplificacion de variables y parametros dinámicos
 
--- Bash Scripting/03_ parametros.sh
+++ Bash Scripting/03_ parametros.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # Author: Leonardo Parlavecchio
 # Como pasar parametros o argumentos
 
